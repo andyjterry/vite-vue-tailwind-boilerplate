1f33af8c121f1b59f91a2feaa98cab2562f61d3b
Andy <andyterry.dev@gmail.com>
2022-12-07 00:05:11 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Vue 3 + Vite + TailwindCSS/SASS Boilerplate
+
+This template should help get you started developing quicker with Vue 3 in Vite along with tailwindCSS and SASS. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+
+## To get started
+
+Once you have cloned the repo, do the following commands
+<pre>npm i</pre>
+<pre>npm run host</pre>
+
+## Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
+
+## Changes
+
+- added 'npm run host' which will then give you a local and network address. (helpful for testing on mobile devices)
+- SASS has been configured. for more information look here https://vitejs.dev/guide/features.html#css
+
+Thanks,
+Andy
+www.andyterry.co.uk
---
+++ src/components/Logos.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  title: String
+})
+
+</script>
+
+<template>
+  <h1 class="text-4xl text-center font-bold mb-10">{{ title }}</h1>
+
+  <div class="logo-row">
+    <!--    Vue Logo-->
+    <a href="https://vuejs.org/" class="group" target="_blank">
+      <img src="/src/assets/vue.svg" alt="Vue logo" />
+      <span class="group-hover:opacity-100">Vite</span>
+    </a>
+    <!--    Vite Logo-->
+    <a href="https://vitejs.dev" class="group" target="_blank">
+      <img src="/src/assets/vite.svg" alt="Vite logo" />
+      <span class="group-hover:opacity-100">Vite</span>
+    </a>
+
+
+    <!--    sass logo-->
+    <a href="https://tailwindcss.com//" class="group" target="_blank">
+      <img src="/src/assets/tailwind.svg" alt="Tailwind Logo" />
+      <span class="group-hover:opacity-100">Tailwind CSS</span>
+    </a>
+    <!--    sass logo-->
+    <a href="https://sass-lang.com/" class="group" target="_blank">
+      <img src="/src/assets/sass.svg" alt="SASS Logo" />
+      <span class="group-hover:opacity-100">SASS Preprocessor</span>
+    </a>
+    <!--    andy logo-->
+    <a href="https://andyterry.co.uk" class="group" target="_blank">
+      <img class="rounded-full" src="/src/assets/andy.svg" alt="Andy logo" />
+      <span class="group-hover:opacity-100 mx-auto">Andy Terry</span>
+    </a>
+
+  </div>
+</template>
+
+
